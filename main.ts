8f426a30bebdac554aa4b59dedd0a1b6b30f1cf0
lucas-nguyen0 <lucas.nguyen@mths.ca>
2024-09-27 10:42:11 -0400

    turns on a a single led

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Lucas Nguyen
+ * Created on: Sep 2024
+ * This program displays light on a signle LED.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+// turn on LED
+input.onButtonPressed(Button.A, function () {
+basic.showIcon(IconNames.Yes)
+pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turn off LED
+input.onButtonPressed(Button.B, function () {
+basic.showIcon(IconNames.No)
+pins.digitalWritePin(DigitalPin.P16, 0)
+})
